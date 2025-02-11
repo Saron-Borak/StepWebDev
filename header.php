9cde6b0cf292c6d8721fbfec057662ba3b4ed10c
SokSothea <sothea2603@gmail.com>
2025-02-11 21:08:07 +0700

    Shop Page

--- header.php
+++ header.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Freschery - Fresh Produce Delivery</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header>
         <nav>
             <div class="logo">
-                <a href="/"><img src="img/logo.png" alt="Freschery"></a>
+                <a href=""><img src="assets/img/logo/logo.png" alt=""></a>
             </div>
             <ul class="nav-links">
-                <li><a href="/shop">Shop</a></li>
+                <li><a href="shop.php">Shop</a></li>
                 <li><a href="/faq">FAQ</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li><a href="/register">Register</a></li>
---
+++ shop.php
@@ -0,0 +1,475 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Shop</title>
+    <link rel="stylesheet" href="shop.css" />
+    <!-- <link rel="stylesheet" href="style.css" /> -->
+
+    <script
+      src="https://kit.fontawesome.com/fefa8fb813.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <?php include 'header.php';?>
+  
+    <section id="title">
+      <div class="container-fluid">
+        <span>Shopping page</span>
+        <p>Sabe time and leave the groceries to us.</p>
+      </div>
+    </section>
+    <!-- carousal -->
+    <section id="Meats">
+      <div class="carsousel_container">
+        <div class="carousel_item">
+          <a href="#" id="angle_left"><i class="fa-solid fa-angle-left"></i></a>
+          <i class="fa-solid fa-carrot"></i>
+          <div class="carousel_Detail">
+            <h2>Vegetables</h2>
+            <p>Lorem ipsum dolor</p>
+          </div>
+        </div>
+
+        <div class="carousel_item">
+          <i class="fa-solid fa-carrot"></i>
+          <div class="carousel_Detail">
+            <h2>Vegetables</h2>
+            <p>Lorem ipsum dolor</p>
+          </div>
+        </div>
+
+        <div class="carousel_item">
+          <i class="fa-solid fa-carrot"></i>
+          <div class="carousel_Detail">
+            <h2>Vegetables</h2>
+            <p>Lorem ipsum dolor</p>
+          </div>
+        </div>
+
+        <div class="carousel_item">
+          <i class="fa-solid fa-carrot"></i>
+          <div class="carousel_Detail">
+            <h2>Vegetables</h2>
+            <p>Lorem ipsum dolor</p>
+          </div>
+          <a href="#" id="angle_right"
+            ><i class="fa-solid fa-angle-right"></i
+          ></a>
+        </div>
+      </div>
+      <!-- Most Want -->
+      <div class="container">
+        <span class="tittle">MOST WANTED</span>
+
+        <!-- card -->
+        <div class="group_card">
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="Vegetables">
+      <!-- Most Want -->
+      <div class="container">
+        <span class="tittle">VEGATABLES</span>
+
+        <!-- card -->
+        <div class="group_card">
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="Meats">
+      <!-- Most Want -->
+      <div class="container">
+        <span class="tittle">MEATS</span>
+
+        <!-- card -->
+        <div class="group_card">
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="fishs">
+      <!-- Most Want -->
+      <div class="container">
+        <span class="tittle">FISHS</span>
+
+        <!-- card -->
+        <div class="group_card">
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="fruit">
+      <!-- Most Want -->
+      <div class="container">
+        <span class="tittle">FRUITS</span>
+
+        <!-- card -->
+        <div class="group_card">
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="ribbon ribbon-top-right"><p>special</p></div>
+            <!-- img -->
+            <div class="warp_img">
+              <img src="assets/img/food2.jpg" alt="vegetables" />
+              <div class="Img_inner">
+                <span class="date">Until 2025</span>
+                <span class="disc">20% OFF</span>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="footer_card">
+              <h5 class="footer_tittle">Vegetables</h5>
+              <h5 class="footer_disc">10$</h5>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <?php include 'footer.php';?>
+  </body>
+</html>
