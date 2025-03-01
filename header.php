8cb0e228a21e46e1a2bb9ec4b8a22d9061211d24
SokSothea <sothea2603@gmail.com>
2025-03-01 19:37:25 +0700

    first commit

--- header.php
+++ header.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Freschery - Fresh Produce Delivery</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <header>
-        <nav>
-            <div class="logo">
-                <a href=""><img src="assets/img/logo/logo.png" alt=""></a>
-            </div>
-            <ul class="nav-links">
-                <li><a href="shop.php">Shop</a></li>
-                <li><a href="/faq">FAQ</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/register">Register</a></li>
-                <li><a href="/login">Login</a></li>
-            </ul>
-        </nav>
-=======
-=======
->>>>>>> 3d3a5e509f599da6ddb925d6e0acfd4bf37c51e3
 <!DOCTYPE html>
 <html lang="en">
 
