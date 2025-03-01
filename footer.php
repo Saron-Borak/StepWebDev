a114f489a464aefd6980932e604161a9972bb0e1
Kyoo <Borakschool@gmail.com>
2025-03-02 02:34:13 +0700

    fixed index, header, and footer

--- footer.php
+++ footer.php
@@ -1,74 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<footer>
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-column">
+                <h3>ABOUT</h3>
+                <p>Nisi esse dolor irure dolor eiusmod ex deserunt proident cillum eu qui enim occaecat sunt aliqua anim eiusmod qui ut voluptate.</p>
+            </div>
 
-    <script src="https://kit.fontawesome.com/fefa8fb813.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="footer.css">
-</head>
-<body>
-  <div class=".container-lg bg-secondary">
-  <footer class="py-5 p-5 overflow-hidden me-5 ms-5">
-    <div class="row"> 
-      <div class="col-6 col-md-2 mb-3">
-        <h5>About</h5>
-        <div class="about_describtion">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, sit! Consequatur, excepturi! Similique quidem, incidunt velit deserunt esse quae optio temporibus blanditiis, voluptas alias maiores eum, quasi vitae aut et.
+            <div class="footer-column">
+                <h3>LINKS</h3>
+                <ul>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="faq.php">FAQ</a></li>
+                    <li><a href="how-it-works.php">How it Works</a></li>
+                    <li><a href="terms.php">Terms</a></li>
+                    <li><a href="privacy.php">Privacy Policy</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-column">
+                <h3>CONTACT</h3>
+                <p><i class="fas fa-phone"></i> 08272367238</p>
+                <p><i class="fas fa-envelope"></i> hello@domain.com</p>
+
+                <h3 class="social-heading">FOLLOW US</h3>
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+
+            <div class="footer-column">
+                <h3>GET OUR APP</h3>
+                <div class="app-links">
+                    <a href="#"><img src="assets/img/playstore.png" alt="Google Play"></a>
+                    <div class="app-preview">
+                        <img src="assets/img/mockup.png" alt="App Preview">
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
 
-      <div class="col-6 col-md-2 mb-3">
-        <h5>Link</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contact Us</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQ</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">How It Work</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Term</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
-        </ul>
-      </div>
-
-      <div class="col-6 col-md-2 mb-3">
-        <h5>Contact</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">📞08272367238</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">💳hello@domain.com</a></li>
-        </ul>
-        <h5>Follow Us</h5>
-        <ul class="nav d-flex">
-          <li class="nav-item m-2"><a href="#" class="nav-link p-0 text-body-secondary"><i  class="fa-brands fa-facebook imgsize"></i></a></li>
-          <li class="nav-item m-2"><a href="#" class="nav-link p-0 text-body-secondary"><i class="fa-brands fa-instagram imgsize"></i></a></li>
-          <li class="nav-item m-2"><a href="#" class="nav-link p-0 text-body-secondary"><i class="fa-brands fa-youtube imgsize"></i></a></li>
-        </ul>
-      </div>
-
-      <div class="col-md-2 offset-md-1 mb-3">
-        <form>
-          <h5>GET OUR APP </h5>
-          <p class="size_playstore"><img src="assets\img\playstore.png" alt="playstore"></p>
-          <div class="d-flex justify-content-end size_mockup">
-            <img src="assets/img/mockup.png" class="rounded d-block" alt="mockup">
-          </div>
-        </form>
-      </div>
     </div>
 
-    <div class="d-flex flex-column flex-sm-row justify-content-center py-4 my-4 border-top">
-      <p>© 2024 Company, Inc. All rights reserved.</p>
-      <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-      </ul>
+    <div class="copyright">
+        <p>&copy; <?php echo date('Y'); ?> Freshcery | Groceries Organic Store. All rights reserved.</p>
     </div>
-  </footer>
-</div>
+</footer>
 
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="scripts.js"></script>
+<?php if (isset($page_specific_js)): ?>
+    <script src="<?php echo $page_specific_js; ?>"></script>
+<?php endif; ?>
 </body>
+
 </html>
